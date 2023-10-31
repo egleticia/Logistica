6a09c8dcfe1c87ce24631193b0bc847dc048bec9
Leticia Egle <leticiaegle@yahoo.com.br>
2023-10-30 22:25:51 -0300

    first commit

---
+++ Logistica.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Logistica.Domain
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Logistica.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Logistica.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
